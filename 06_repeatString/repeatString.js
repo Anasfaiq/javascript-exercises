8d3d2a82e40de32c903f0734be8ec0ed0639085a
Anas Faiq <176678988+Anasfaiq@users.noreply.github.com>
2025-10-21 16:31:19 +0700

    Repeat function selesai

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, rep) {
+  result = '';
 
+  if(rep < 0) {
+    return "ERROR"
+  }else {
+    for(let i = 0; i < rep ; i++){
+    result += string;
+    }
+    return result;
+  }
 };
+
+repeatString("hello",10)
 
 // Do not edit below this line
 module.exports = repeatString;
