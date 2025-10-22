b7f1a1dbe0ebef41322a52c3365f40f1064c19aa
Anas Faiq <176678988+Anasfaiq@users.noreply.github.com>
2025-10-22 10:12:11 +0700

    sumAll function selesai

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+  // checking if the num is positive and integer
+  if(startNum < 0 || endNum < 0 || !Number.isInteger(startNum) || !Number.isInteger(endNum)){
+    return "ERROR";
+  } else {
+    startNum && endNum
+  }
+  
+  //checking if the array start from smaller number to larger number
+  if(startNum > endNum){
+    let temp = startNum;
+    startNum = endNum;
+    endNum = temp;
+  }
 
+
+  let result = [];
+
+  for (let i = startNum; i <= endNum; i++) {
+    result.push(i)
+  }
+  return result.reduce((total, currentValue) => {
+    return total + currentValue;
+  }, 0)
 };
 
 // Do not edit below this line
