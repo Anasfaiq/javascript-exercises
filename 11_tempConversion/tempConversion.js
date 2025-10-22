3adbd5aa4db3288cb8b954e39fc5bbf4cac5d794
Anas Faiq <176678988+Anasfaiq@users.noreply.github.com>
2025-10-22 11:10:04 +0700

    tempConversion function selesai

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  result = (tempF - 32) * 0.5556
+  rounded = parseFloat(result.toFixed(1));
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  result = (tempC * 1.8) + 32;
+  rounded = parseFloat(result.toFixed(1));
+  return rounded;
 };
 
 // Do not edit below this line
