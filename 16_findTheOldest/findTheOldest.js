acf0ff17905a3ae9f11e536203c5b08a47e487fd
Anasfaiq <176678988+Anasfaiq@users.noreply.github.com>
2025-11-10 14:58:31 +0700

    Selesai

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  const currentYear = new Date().getFullYear();
+  let oldestPerson = arr.reduce((oldest, person) => {
+    const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+    const personAge = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+    return personAge > oldestAge ? person : oldest;
+  });
+  return oldestPerson;
 };
 
 // Do not edit below this line
